3ee7ef0f4bf8189df475b77b77dcf856f7e3ddaa
and <and@and>
2025-07-22 20:12:47 -0300

    criado view de renderização do template lista herois (heroes/views.py) e REGISTRADO no heroes/urls.py

--- heroes/urls.py
+++ heroes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.hello_heroes, name='hello_heroes'),
+    path('lista/', views.lista_herois, name='lista_herois'),
 ]
--- heroes/views.py
+++ heroes/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Hero
 
-def hello_heroes(request):
-    return HttpResponse("Bem-vindo ao módulo Heroes!")
+def lista_herois(request):
+    herois = Hero.objects.all()  # busca todos os heróis do banco
+    return render(request, "heroes/lista_herois.html", {"herois": herois})
 
 # Create your views here.
