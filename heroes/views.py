bd1f18bb80eec5b0e7ac724d3698190e773fb133
and <and@and>
2025-07-22 19:17:09 -0300

    exemplo simples de uma view parte 1 - hello heroes

--- heroes/views.py
+++ heroes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def hello_heroes(request):
+    return HttpResponse("Bem-vindo ao módulo Heroes!")
 
 # Create your views here.
